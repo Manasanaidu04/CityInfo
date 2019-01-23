8bb243942800cd7feddf1a074e31042b504db9f1
Manasa Ramisetti <manasa.naidu04@gmail.com>
2019-01-23 16:05:19 -0600

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,85 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {                
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park",
+                    PointOfInterests = new List<PointOfInterestsDto>()
+                    {
+                        new PointOfInterestsDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the US."
+                        },
+                        new PointOfInterestsDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-sotry building located in Midtown Manhattan"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with cathedral that was never really finished",
+                    PointOfInterests = new List<PointOfInterestsDto>()
+                    {
+                        new PointOfInterestsDto()
+                        {
+                            Id = 1,
+                            Name = "Cathedral of Our Lady",
+                            Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans"
+                        },
+                        new PointOfInterestsDto()
+                        {
+                            Id = 2,
+                            Name = "Antwerp Central Station",
+                            Description = "The finest example of railwau atchitecture in Belguim."
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower",
+                    PointOfInterests = new List<PointOfInterestsDto>()
+                    {
+                        new PointOfInterestsDto()
+                        {
+                            Id = 1,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Cham de Mars, named after engineer Gustave Eiffel."
+                        },
+                        new PointOfInterestsDto()
+                        {
+                            Id = 2,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        }
+                    }
+                }
+
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,206 @@
+﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        private ILogger<PointsOfInterestController> _logger;
+        private IMailService _mailService;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger,
+            IMailService mailService)
+        {
+            _logger = logger;
+            _mailService = mailService;
+        }
+
+
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointOfInterest(int cityId)
+        {
+            try
+            {
+                throw new Exception("Exception sample");
+                var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+                if (city == null)
+                {
+                    _logger.LogInformation($"City with Id {cityId} wasn't found when accessing points of interest");
+                    return NotFound();
+                }
+
+                return Ok(city.PointOfInterests);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points od interest for city with id {cityId}", ex);
+                return StatusCode(500, "A problem happened while handling your request");
+            }
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointOfInterests.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+                return NotFound();
+
+            return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointOfInterests).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestsDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+
+            };
+
+            city.PointOfInterests.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId, id = finalPointOfInterest.Id }, finalPointOfInterest);
+
+        }
+
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id, [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterests.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+                return NotFound();
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{cityId}/pointsofinterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterests.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+                return NotFound();
+
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+            {
+                Name = pointOfInterestFromStore.Name,
+                Description = pointOfInterestFromStore.Description
+            };
+
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (pointOfInterestToPatch.Name == pointOfInterestToPatch.Description)
+                ModelState.AddModelError("Description", "Description should be different from the Name.");
+
+            TryValidateModel(pointOfInterestToPatch);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterests.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+                return NotFound();
+
+            city.PointOfInterests.Remove(pointOfInterestFromStore);
+
+            _mailService.Send("Point of interest deleted",
+                $"Point of interest {pointOfInterestFromStore.Name} with id {pointOfInterestFromStore.Id} was deleted");
+
+            return NoContent();
+        }
+    }
+}
